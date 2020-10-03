a0847e9eacd14358b36610bfeb185c462440021a
AdhamAbuRamadan <37475344+AdhamAbuRamadan@users.noreply.github.com>
2020-10-03 22:52:55 +0300

    a

---
+++ MyApplication5/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public final class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(final Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+       final int x = 11;
+          int n = 1;
+        x * 2 = n;
+    }
+}
